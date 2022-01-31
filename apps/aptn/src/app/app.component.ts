4ae8af748edfeeaa2fb6ed2ea6973ca46379128a
MAmir090 <mohamad.amir.090@gmail.com>
2022-01-31 17:58:05 +0200

    initial commit

---
+++ apps/aptn/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'refactor-ott-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'aptn';
+}
